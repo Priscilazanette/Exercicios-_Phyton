676314dd0e1de67fbca3664e4adc32aa5327190c
priscila zanette <121053628+Priscilazanette@users.noreply.github.com>
2024-04-03 18:49:33 -0300

    Importando da Biblioteca
    Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção Inteira.

---
+++ Ex06.py
@@ -0,0 +1,8 @@
+#importando da biblioteca
+'''from math import trunc
+num = float(input('Digite a parte inteira'))
+print('O valor digitado foi {} e a sua porção inteira é {}'.format(num, trunc(num)))'''
+
+#Sem importar da Biblioteca
+num = float(input('Digite um numero'))
+print('O numero digitado foi {} e a sua porção inteira é {}'.format(num, int(num)))
